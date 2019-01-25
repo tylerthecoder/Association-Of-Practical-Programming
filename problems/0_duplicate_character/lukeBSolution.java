0caaf43ccedf51246a3b89c286516731b92ebc42
LukeBrandon <lukebrandon68@gmail.com>
2019-01-25 14:16:52 -0600

    completed java problem 1

--- problems/0_duplicate_character/lukeBSolution.java
+++ problems/0_duplicate_character/lukeBSolution.java
@@ -1,25 +1,37 @@
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 class lukeBSolution{
 
-    string checkDouble(String word){
+    static String checkDouble(String word){
         Set<Integer> hashSet = new HashSet<Integer>(); 
 
-        for(int i =0; i < word.length; i++){
-            if(hashSet.contains(word[i]))
+        //goes through each letter and if the hashset already has that character then returns true for has a double and false otherwise
+        for(int i =0; i < word.length(); i++){
+            if(hashSet.contains((int)word.charAt(i)) )
                 return "true";
-            hashSet.add(word[i]); 
+            hashSet.add((int)word.charAt(i)); 
         }
-        
         return "false";
     }
 
 
     public static void main(String [] args){
-        //read file
+        //reads file
+        File file = new File("input.txt");
+        try{
+            Scanner scanner = new Scanner(file);
 
-        while(/*not end of file*/){
-            checkDouble(/*word*/);
-        }
-    }
-}
+            //runs each word through the checkDouble method
+            while(scanner.hasNextLine()){
+                System.out.println( checkDouble(scanner.nextLine()) );
+            }
+
+        }catch(FileNotFoundException e){
+            System.out.println("file not found");
+
+        } 
+
+    } //end main method
+} //end class
