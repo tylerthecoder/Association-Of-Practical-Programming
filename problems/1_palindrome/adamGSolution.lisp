a6db9f42d4171e38bb18884d0344aa549d8ebf4a
AdamGoertz <36753247+AdamGoertz@users.noreply.github.com>
2019-01-27 13:08:28 -0600

    Challenge accepted (#11)
    * Challenge Accepted & Completed
    
    Added a LISP Solution
    
    * Edited file paths

---
+++ problems/1_palindrome/adamGSolution.lisp
@@ -0,0 +1,35 @@
+;define functions
+(defun palindrome-checker (str) 
+  (equal str (reverse str)))
+
+
+(defun read-strings (filename)
+  (with-open-file (stream filename)
+    (loop for line = (read-line stream nil)
+          while line
+          collect (string-right-trim '(#\return #\newline) line))))
+
+
+(defun convert-boolean (bool)
+  (if (equal bool "false") nil t))
+
+
+(defun test-checker (string soln)
+  (equal (palindrome-checker string) soln))
+
+
+                            
+(defvar *tests*) ;define global variables
+(defvar *solns*)
+(setf *tests* (read-strings "%PATH_TO_GIT_REPO%/Association-Of-Practical-Programming/problems/1_palindrome/input.txt")) ; read files
+(setf *solns* (mapcar #'convert-boolean (read-strings "%PATH_TO_GIT_REPO%/Association-Of-Practical-Programming/problems/1_palindrome/output.txt")))
+
+
+(format t "~{~a ~}" (mapcar #'test-checker *tests* *solns*)) ;output results
+
+
+
+
+
+
+
