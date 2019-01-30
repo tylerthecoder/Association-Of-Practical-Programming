b367e61590e650c1dc4f2857bd353549dd9db437
Tyler Tracy <me@tylertracy.com>
2019-01-30 07:36:08 -0600

    Added message board componnet to angular

--- meetings/1_frameworks/angular/prep-app/src/app/app.module.ts
+++ meetings/1_frameworks/angular/prep-app/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CageSlideComponent } from './cage-slide/cage-slide.component';
+import { MessageBoardComponent } from './message-board/message-board.component';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://api.tgt101.com:4444', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent,
-    CageSlideComponent
+    CageSlideComponent,
+    MessageBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
